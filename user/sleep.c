73066ef3f67b083cc2a4e95e1050b487da472779
Pikachudy <923252409@qq.com>
2022-07-27 23:05:10 +0800

    初始化项目，完成 Utilities-Sleep

---
+++ user/sleep.c
@@ -0,0 +1,24 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int 
+main(int argc,char *argv[]){
+    if(argc<=1){
+        fprintf(2,"error:Please input the ticks you want to sleep ");
+    }
+    else if(argc==2){
+        int time=atoi(argv[1]);
+        if(time<=0){
+            fprintf(2,"error:Please input valid time");
+        }
+        else{
+            sleep(time);
+            exit(1);
+        }
+    }
+    else{
+        fprintf(2,"error:Please input no more than one time value");
+    }
+    exit(0);
+}
